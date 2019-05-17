9a4cfac95925ae364b6be2f64318e4e6e7a98d2f
xinghouliu <xinghouliu@datamesh.com>
2019-05-17 19:06:18 +0800

    是是是

---
+++ src/main/java/com/steve/algorithms/DynamicPlan.java
@@ -0,0 +1,75 @@
+package com.steve.algorithms;
+
+public class DynamicPlan {
+
+    public static void main(String[] args) {
+        int[] p = {1,2, 5, 10};
+//        System.out.println(test1(p));
+        System.out.println(test3(p));
+    }
+
+    /**
+     * 区间模型： 线性模型. 过桥问题
+     * 假设p是排好序的，则通式为： 重叠问题, 第i个人过桥，两种情况， 一个人过桥，两个人过桥
+     *    dp[i] = dp[i-1] + p[0] + p[i]
+     *    dp[i] = dp[i-2] + p[0] + 2*p[1] + p[i]
+     * @param p 花费时间排序的
+     * @return
+     */
+    public static int test3(int[] p){
+        int[] dp = new int[p.length];
+        dp[0] = p[0];
+        dp[1] = p[1];
+        for(int i=2; i<p.length ;i++){
+            int min = Math.min(dp[i-1]+ p[0] + p[i], dp[i-2] + p[0] +p[1]*2 + p[i]);
+        dp[i] = min;
+        }
+        return dp[p.length - 1];
+    }
+    /**
+     * 这个问题是 Rn = Pi + R(n-i)
+     * @param p
+     * @return
+     */
+    public static int test2(int[] p){
+        int n = p.length;
+        int[] r = new int[n+1];
+        for(int i=1; i<= p.length; i++){
+            int q = -2;
+            for(int j=1 ; j <= i; j++){
+                q = Math.max(q, r[i - j] + p[j -1]);   // 求的是 r[n] 的最优解
+            }
+            r[i] = q;
+        }
+        return r[p.length];
+    }
+
+    /**
+     * n 长度的钢条和 p 类型的价格表
+     * 自顶而下 备忘录法
+     * @param p
+     */
+    public static int test1(int[] p){
+        int[] dp = new int[p.length + 1];
+        for(int i=0; i <= p.length ;i ++){
+            dp[i] = -1;
+        }
+        return cut(p, p.length, dp);
+    }
+
+    private static int cut(int[] p, int n, int[] dp) {
+        int max = -1;
+        if(dp[n] > -1){
+            return dp[n];
+        }
+        if(n == 0){
+            max = 0;  // 不切割的话，默认就是 Pn 的价格，所以这里为0
+        } else {
+            for(int i=1; i <= n; i++){
+                max = Math.max(max, cut(p, n - i, dp) + p[i-1]);
+            }
+        }
+        dp[n] = max;
+        return max;
+    }
+}
