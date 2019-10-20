c239dcbc821be236931bf4912088d1ecaf86c1dd
steve <995270418@qq.com>
2019-10-20 23:37:49 +0800

    update

---
+++ src/main/java/com/steve/algorithms/Question_33.java
@@ -0,0 +1,20 @@
+package com.steve.algorithms;
+
+/**
+ *
+ *
+ */
+public class Question_33 {
+
+    public int search(int[] nums, int target) {
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        Question_33 question = new Question_33();
+        int[] nums = {1, 3, 2};
+        System.out.println(question.search(nums, 2));
+    }
+
+}
