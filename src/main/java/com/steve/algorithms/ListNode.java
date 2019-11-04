7576c987bfeed4751094802150cf73107c54d8da
steve <995270418@qq.com>
2019-11-04 23:44:28 +0800

    update

--- src/main/java/com/steve/algorithms/ListNode.java
+++ src/main/java/com/steve/algorithms/ListNode.java
@@ -3,13 +3,11 @@ package com.steve.algorithms;
 
 public class ListNode {
 
-    public static int val;
-    public static ListNode next;
+    public int val;
+    public ListNode next;
 
     public ListNode(int val) {
         this.val = val;
-        this.next = null;
     }
-
 
 }
