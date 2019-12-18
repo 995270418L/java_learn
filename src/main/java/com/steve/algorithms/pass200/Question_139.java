d027863b36ee456d1537af4908ece80f0994f2f5
steve <995270418@qq.com>
2019-12-18 23:48:30 +0800

    update

--- src/main/java/com/steve/algorithms/pass200/Question_139.java
+++ src/main/java/com/steve/algorithms/pass200/Question_139.java
@@ -1,5 +1,6 @@
 package com.steve.algorithms.pass200;
 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -29,15 +30,45 @@ import java.util.List;
  */
 public class Question_139 {
 
+    /**
+     * dp 1
+     * @param s
+     * @param wordDict
+     * @return
+     */
     public boolean wordBreak(String s, List<String> wordDict) {
-        // todo tomorrow
-        return false;
+        if(s == null) return true;
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for(int i=0; i<s.length(); i++){
+            for(int j=i+1; j<=s.length(); j++){
+                if(dp[i] && wordDict.contains(s.substring(i, j))){
+                    dp[j] = true;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+
+    public boolean wordBreak2(String s, List<String> wordDict) {
+        if(s == null) return true;
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for(int i=1; i<=s.length(); i++){
+            for(int j=0; j< i; j++){
+                if(dp[j] && wordDict.contains(s.substring(j, i))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
     }
 
     public static void main(String[] args) {
         Question_139 question = new Question_139();
         char[][] src = {{'X','X','X','X'},{'X','O','O','X'},{'X','X','O','X'},{'X','O','X','X'}};
-        System.out.println(question.wordBreak("", null));
+        System.out.println(question.wordBreak2("aaaaaaa", new LinkedList<String>(){{add("aaaa"); add("aaa");}}));
     }
 
 }
