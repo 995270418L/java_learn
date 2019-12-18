817b9af493f2c28973482cdf1046b063af198abe
steve <995270418@qq.com>
2019-12-19 00:19:41 +0800

    update

--- src/main/java/com/steve/algorithms/pass200/Question_142.java
+++ src/main/java/com/steve/algorithms/pass200/Question_142.java
@@ -2,71 +2,67 @@ package com.steve.algorithms.pass200;
 
 import com.steve.algorithms.base.ListNode;
 
-import java.util.LinkedList;
-import java.util.List;
-
 /**
- * 简单:
  *
- * 给定一个链表，判断链表中是否有环。
+ * 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
  *
  * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+ *
+ * 说明：不允许修改给定的链表。
  *
  *  
  *
  * 示例 1：
  *
  * 输入：head = [3,2,0,-4], pos = 1
- * 输出：true
+ * 输出：tail connects to node index 1
  * 解释：链表中有一个环，其尾部连接到第二个节点。
  *
  *
  * 示例 2：
  *
  * 输入：head = [1,2], pos = 0
- * 输出：true
+ * 输出：tail connects to node index 0
  * 解释：链表中有一个环，其尾部连接到第一个节点。
  *
  *
  * 示例 3：
  *
  * 输入：head = [1], pos = -1
- * 输出：false
+ * 输出：no cycle
  * 解释：链表中没有环。
  *
- *
- *  
- *
  * 进阶：
- *
- * 你能用 O(1)（即，常量）内存解决此问题吗？
+ * 你是否可以不用额外空间解决此题？
  *
  */
-public class Question_141 {
+public class Question_142 {
 
-    /**
-     * 双指针, 只需证明总会相遇
-     * @param head
-     * @return
-     */
-    public boolean hasCycle(ListNode head) {
-        if(head == null || head.next == null) return false;
+    public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next == null) return null;
+        ListNode ptr1=head;
+        ListNode ptr2; // slow and fast pointer 第一次相遇的点
         ListNode slow = head;
         ListNode fast = head;
-        int meet = 0;
-        while (slow != fast || meet == 0){
-            if(fast == null || fast.next == null) return false;
+        int index = 0;
+        while (slow != fast || index == 0){
+            if(fast == null || fast.next == null) return null;
             slow = slow.next;
             fast = fast.next.next;
-            meet = 1;
+            index = 1;
         }
-        return true;
+        ptr2 = slow;
+        while(ptr1 != ptr2){
+            ptr1 = ptr1.next;
+            ptr2 = ptr2.next;
+        }
+        return ptr1;
     }
 
     public static void main(String[] args) {
-        Question_141 question = new Question_141();
+        Question_142 question = new Question_142();
         char[][] src = {{'X','X','X','X'},{'X','O','O','X'},{'X','X','O','X'},{'X','O','X','X'}};
-        System.out.println(question.hasCycle(null));
+        System.out.println(question.detectCycle(null));
     }
 
 }
