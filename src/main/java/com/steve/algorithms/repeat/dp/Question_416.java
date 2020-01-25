8dbdd3f218addb593a17222f1431910120205a39
steve <995270418@qq.com>
2020-01-25 22:13:17 +0800

    update

--- src/main/java/com/steve/algorithms/repeat/dp/Question_416.java
+++ src/main/java/com/steve/algorithms/repeat/dp/Question_416.java
@@ -4,6 +4,7 @@ package com.steve.algorithms.repeat.dp;
 public class Question_416 {
 
     public boolean canPartition(int[] nums) {
+
         return false;
     }
 
@@ -19,7 +20,10 @@ public class Question_416 {
      *  dp[i][j] = Math.max(dp[i-1][j], dp[i-1][V-c[i]] + v[i])  // 不放入 i 件物品,空间占用沿用上一个的，容量也是 j，放入第 i 件物品， 剩余容量 V- c[i] 的背包中，
      *  此时的最大价值是 dp[i-1][V - c[i]] 和 v[i] 的和
      *
-     * 3. 边界条件
+     * 3.
+     *  空间压缩
+     *  V
+     *
      *
      */
     public int packageZeroToOne(int V, int[] c, int[] v) {
@@ -38,9 +42,24 @@ public class Question_416 {
         return dp[size][V];
     }
 
+    public int packageZeroToOne2(int V, int[] c, int[] v) {
+        int size = v.length;
+        int[] dp = new int[V+1];
+        for (int i = 1; i <= size; i++) {
+            for (int j = V; j > 0; j--) {
+                if (c[i - 1] > j) {
+                    dp[j] = dp[j-1];  // 不放入这个物品
+                } else {
+                    dp[j] = Math.max(dp[j-1], dp[j - c[i - 1]] + v[i - 1]);
+                }
+            }
+        }
+        return dp[V];
+    }
+
     public static void main(String[] args) {
         Question_416 question = new Question_416();
-        int res = question.packageZeroToOne(10, new int[]{2,2,6,5,4}, new int[]{6,3,5,4,6});
+        int res = question.packageZeroToOne2(10, new int[]{2,2,6,5,4}, new int[]{6,3,5,4,6});
         System.out.println(res);
     }
 
