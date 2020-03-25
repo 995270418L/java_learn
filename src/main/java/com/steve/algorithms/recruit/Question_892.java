c15f47e5e38e57251a05d34b306ebe01bbc0a266
steve <995270418@qq.com>
2020-03-25 23:37:47 +0800

    update

---
+++ src/main/java/com/steve/algorithms/recruit/Question_892.java
@@ -0,0 +1,21 @@
+package com.steve.algorithms.recruit;
+
+/**
+ *
+ * 892. 三维形体的表面积
+ *
+ */
+public class Question_892 {
+
+    public int surfaceArea(int[][] grid) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Question_892 question = new Question_892();
+        int[] arr = {0, 0, 1, 2, 4, 2 ,2, 3, 1, 4};
+        int target = question.surfaceArea(null);
+        System.out.println(target);
+    }
+
+}
