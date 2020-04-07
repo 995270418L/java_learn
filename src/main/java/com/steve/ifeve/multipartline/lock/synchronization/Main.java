22fa1502c83a70767d5e002b2642978bb82ce680
steve <995270418@qq.com>
2020-04-07 23:32:33 +0800

    update

--- src/main/java/com/steve/ifeve/multipartline/lock/synchronization/Main.java
+++ src/main/java/com/steve/ifeve/multipartline/lock/synchronization/Main.java
@@ -2,6 +2,12 @@ package com.steve.ifeve.multipartline.lock.synchronization;
 
 public class Main {
 
+    /**
+     * 同一个对象中，并发的时候多线程会不会并发访问对象中没加synchorized修饰符的方法 ？
+     * 不会， 因为synchorized只会保护对象的指定代码块里面的代码不会并发访问。
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         Thread[] thread = new Thread[10];
         Sync sync = new Sync();
--- src/main/java/com/steve/ifeve/multipartline/lock/synchronization/Main2.java
+++ src/main/java/com/steve/ifeve/multipartline/lock/synchronization/Main2.java
@@ -4,17 +4,17 @@ import org.openjdk.jol.info.ClassLayout;
 
 public class Main2 {
 
+    /**
+     * Synchorized 的指令代码实现
+     *
+     * @param args
+     * @throws InterruptedException
+     */
     public static void main(String[] args) throws InterruptedException {
-        Thread.sleep(5000);
-        Native o = new Native();
-        System.out.println(ClassLayout.parseInstance(o).toPrintable());
-//        synchronized (o) {   // 清量级锁
-//            System.out.println(ClassLayout.parseInstance(o).toPrintable());
-//        }
-    }
-
-    static class Native {
-
+        Object o = new Object();
+        synchronized (o) {   // 轻量级锁
+            System.out.println(ClassLayout.parseInstance(o).toPrintable());
+        }
     }
 
 }
