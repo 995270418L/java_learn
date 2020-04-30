ae3780fccc59b96d3d06839810ad3817a732f3af
steve <995270418@qq.com>
2020-05-01 00:02:16 +0800

    update

--- src/main/java/com/steve/ifeve/multipartline/Main.java
+++ src/main/java/com/steve/ifeve/multipartline/Main.java
@@ -1,7 +1,6 @@
 package com.steve.ifeve.multipartline;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by liu on 4/9/17.
@@ -9,10 +8,23 @@ import java.util.Map;
 public class Main {
 
     public static void main(String... args) {
-        Map<Integer, String> map = new HashMap<>(1);
+        ConcurrentHashMap<Integer, Integer> map = new ConcurrentHashMap<>();
         for (int i = 0; i < 20; i++) {
-            map.put(i + 1, "name_" + i);
+            map.put(i + 1, i);
         }
+    }
+
+    static class Node {
+        public int key;
+        public int value;
+        public Node next;
+
+        public Node(int key, int value, Node next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
     }
 
     /**
